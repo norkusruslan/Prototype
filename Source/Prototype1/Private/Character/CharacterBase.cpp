ba6bde0e3b6d783eface66bf5d23a052390e5e60
Ruslan Norkus <norkusruslan@yandex.ru>
2024-07-31 22:07:46 +0200

    Player and Enemy Character

---
+++ Source/Prototype1/Private/Character/CharacterBase.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CharacterBase.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
